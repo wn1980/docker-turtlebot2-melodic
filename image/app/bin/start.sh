36aa0f510935a6b0d1d77ae25aa6372938cb4a0d
wn1980 <waipotn@hotmail.com>
2021-12-19 20:03:45 +0700

    update start.sh

--- image/app/bin/start.sh
+++ image/app/bin/start.sh
@@ -6,4 +6,6 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 
 roslaunch turtlebot_bringup minimal.launch &
 
-roslaunch turtlebot_bringup 3dsensor.launch 
+roslaunch turtlebot_bringup 3dsensor.launch &
+
+tail -f /dev/null
