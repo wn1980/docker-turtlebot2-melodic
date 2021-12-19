2a4225f205b4ad084dc3ba1d7342a54513467ae7
wn1980 <waipotn@hotmail.com>
2021-12-20 05:56:13 +0700

    add don't remove

--- image/app/bin/start.sh
+++ image/app/bin/start.sh
@@ -2,10 +2,14 @@
 
 set -e
 
+############# DON'T REMOVE ############
 source /opt/ros/$ROS_DISTRO/setup.bash
+#######################################
 
 roslaunch turtlebot_bringup minimal.launch &
 
 roslaunch turtlebot_bringup 3dsensor.launch &
 
+###### DON'T REMOVE ######
+# run forever
 tail -f /dev/null
